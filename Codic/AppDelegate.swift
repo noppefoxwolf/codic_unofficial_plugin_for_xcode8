f68b0f5e17dc53cbf21f6d6aa0b246715853a20e
Tomoya Hirano <tomoya.hirano@dena.com>
2016-07-09 20:14:03 +0900

    Initial Commit

---
+++ Codic/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Codic
+//
+//  Created by Tomoya Hirano on 2016/07/09.
+//  Copyright © 2016年 Tomoya Hirano. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Codic/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Codic
+//
+//  Created by Tomoya Hirano on 2016/07/09.
+//  Copyright © 2016年 Tomoya Hirano. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
